63304c686ea3f6d00aed582848011d17af7e21dd
Samir-Durga-CC <samir.d@communicationcrafts.com>
2026-09-25 18:50:05 +0530

    add new changes

--- fastapi-basics/main.py
+++ fastapi-basics/main.py
@@ -120,4 +120,4 @@ def delete_student(student_id: int):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=1234, reload=True)
